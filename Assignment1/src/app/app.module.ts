a648b32d9be7d8d6a839e6ec293ddfc171fe7688
JoshuaRobinson2197 <55886059+JoshuaRobinson2197@users.noreply.github.com>
2020-10-27 15:03:59 -0400

    Completed Assignment 4

--- Assignment1/src/app/app.module.ts
+++ Assignment1/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { StringTypePipePipe } from './string-type-pipe.pipe';
+import { HoverStyleDirective } from './hover-style.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    StringTypePipePipe
+    StringTypePipePipe,
+    HoverStyleDirective
   ],
   imports: [
     BrowserModule
